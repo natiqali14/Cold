802af0d6131ca1e5f53a05153b7c01cc7bfe0a81
Natiq Ali Syed <natiq.ali@msd-my.com>
2024-08-12 21:10:54 +0500

    shader system in progress

--- src/opengl_backend/Shaders/ShaderSystem.cpp
+++ src/opengl_backend/Shaders/ShaderSystem.cpp
@@ -1,6 +1,67 @@
 #include "ShaderSystem.h"
-
+#include <Logger.h>
 namespace Cold
 {
-    
+#define CHECK_FOR_GLOBAL_UNIFORM(instance_map, name) { \
+    COLD_ASSERT(instance_map.count(name) > 0, "Failed to have a global uniform buffer of that name") \
+}
+
+    ShaderSystem* instance = nullptr;
+    void ShaderSystem::initiate()
+    {
+        instance = new ShaderSystem;
+        COLD_TRACE("Shader System Initialse");
+    }
+    void ShaderSystem::shut_down()
+    {
+        delete instance;
+        instance = nullptr;
+        COLD_TRACE("Shader System Shut Down");
+    }
+
+    void ShaderSystem::global_uniform_buffer_object_add(void *data, u32 size, const std::string &name)
+    {
+        UniformBufferSPtr buffer =  std::make_shared<UniformBuffer>();
+        buffer->data = data;
+        buffer->size = size;
+        instance->global_uniform_buffer_objects.insert({name, buffer});
+    }
+
+    void ShaderSystem::global_uniform_buffer_object_update(const std::string& name, void *sub_data, u32 size, u32 offset)
+    {
+        CHECK_FOR_GLOBAL_UNIFORM(instance->global_uniform_buffer_objects, name);
+        UniformBufferSPtr buffer = instance->global_uniform_buffer_objects[name];
+        buffer->add_sub_data_to_pass_to_gpu(sub_data, size, offset);
+    }
+
+    void ShaderSystem::global_uniform_buffer_objects_pass_to_gpu()
+    {
+        
+    }
+
+    ShaderId ShaderSystem::create_shader()
+    {
+        return ShaderId();
+    }
+    void ShaderSystem::shaders_compile(ShaderId shader_id, const char *vertex_shader, const char *fragment_shader)
+    {
+    }
+    void ShaderSystem::uniform_object_add(ShaderId shader_id, const std::string &uniform, const std::any &value)
+    {
+    }
+    void ShaderSystem::uniform_object_update(ShaderId shader_id, const std::string &uniform, const std::any &value)
+    {
+    }
+    void ShaderSystem::uniform_objects_pass_to_gpu(ShaderId shader_id)
+    {
+    }
+    void ShaderSystem::uniform_buffer_object_add(ShaderId shader_id, void *data, u32 size, const std::string &name)
+    {
+    }
+    void ShaderSystem::uniform_buffer_object_update(ShaderId shader_id, void *sub_data, u32 size, u32 offset)
+    {
+    }
+    void ShaderSystem::uniform_buffer_objects_pass_to_gpu(ShaderId shader_id)
+    {
+    }
 }
--- src/opengl_backend/Shaders/ShaderSystem.h
+++ src/opengl_backend/Shaders/ShaderSystem.h
@@ -4,8 +4,46 @@
 #include <any>
 namespace Cold {
     using ShaderId = u32;
+    struct UniformObject {
+        std::any value;
+        EUniformType tpye;
+    };
+
+    struct UniformBuffer {
+        struct SubDataBuffering {
+            void* sub_data;
+            u32 offset;
+            u32 size;
+        };
+        void* data;  // TODO right now saving the pointer to data here. No idea how to make it better
+        u32 size;
+        u32 uniform_buffer_u_id {0};
+        std::vector<SubDataBuffering> data_to_update; // this vector used to pass sub data on buffer update.
+        bool should_pass_data {true};
+
+        void add_sub_data_to_pass_to_gpu(void* sub_data, u32 size, u32 offset) {
+            data_to_update.push_back({ sub_data, offset, size });
+        }
+    };
+    using UniformBufferSPtr = std::shared_ptr<UniformBuffer>;
+
     struct Shader{
         u32 program_id;
+        // data of uniforms to a shader has.
+        std::unordered_map<std::string, UniformObject> uniform_objects;
+        // data that needs to be passed to gpu
+        std::vector<std::string> uniforms_to_pass_to_gpu;
+
+        // unifrom buffers to pass to the shader
+        std::unordered_map<u32, UniformBufferSPtr> uniform_buffer_objects;
+        // uniform buffers to pass to GPU
+        std::vector<u32> uniform_buffers_to_pass_to_gpu;
+
+        const char* vertex_shader;
+        const char* fragment_shader;  // TODO right now only vertex and fragment shaders
+
+
+
 
     };
 
@@ -25,12 +63,23 @@ namespace Cold {
     public:
         static void initiate();
         static void shut_down();
-        static void set_global_uniform_buffer_object();
+        static void global_uniform_buffer_object_add(void* data, u32 size, const std::string& name);
+        static void global_uniform_buffer_object_update(const std::string& name, void* sub_data, u32 size, u32 offset);
+        static void global_uniform_buffer_objects_pass_to_gpu();
         static ShaderId create_shader();
-        static void compile_shaders(ShaderId shader_id, const char* vertex_shader, const char* fragment_shader);
-        static void pass_uniform(ShaderId shader_id, std::any value);
+        static void shaders_compile(ShaderId shader_id, const char* vertex_shader, const char* fragment_shader);
+        static void uniform_object_add(ShaderId shader_id, const std::string& uniform, const std::any& value);
+        static void uniform_object_update(ShaderId shader_id, const std::string& uniform, const std::any& value);
+        static void uniform_objects_pass_to_gpu(ShaderId shader_id);
+        static void uniform_buffer_object_add(ShaderId shader_id, void* data, u32 size, const std::string& name);
+        static void uniform_buffer_object_update(ShaderId shader_id, void* sub_data, u32 size, u32 offset);
+        static void uniform_buffer_objects_pass_to_gpu(ShaderId shader_id);
         
     private:
         std::unordered_map<ShaderId, Shader> shaders_data;
+        // unifrom buffers to pass to the shader
+        std::unordered_map<std::string, UniformBufferSPtr> global_uniform_buffer_objects;
+        // uniform buffers to pass to GPU
+        std::vector<std::string> global_uniform_buffers_to_pass_to_gpu;
     };
 }
